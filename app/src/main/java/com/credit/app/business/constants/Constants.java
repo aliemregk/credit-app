324aae7287335244490ecccb71d9505160d572b3
aliemregk <aliemregk@outlook.com>
2023-02-18 12:56:17 +0300

    Add constants

---
+++ app/src/main/java/com/credit/app/business/constants/Constants.java
@@ -0,0 +1,23 @@
+package com.credit.app.business.constants;
+
+public final class Constants {
+    public static final int CREDIT_LIMIT_MULTIPLIER = 4;
+
+    public static final int CREDIT_SCORE_MIN = 500;
+    public static final int CREDIT_SCORE_LIMIT = 1000;
+
+    public static final double INCOME_MIN = 5000;
+    public static final double INCOME_MAX = 10000;
+
+    public static final double MIN_CREDIT_AMOUNT = 10000;
+    public static final double MID_CREDIT_AMOUNT = 20000;
+
+    public static final double MIN_GUARANTEE = 10 / 100;
+    public static final double LOW_GUARANTEE = 20 / 100;
+    public static final double MID_GUARANTEE = 25 / 100;
+    public static final double MAX_GUARANTEE = 50 / 100;
+
+    private Constants() {
+    }
+
+}
